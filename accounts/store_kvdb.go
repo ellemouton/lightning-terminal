e454ac25e8b03e6cf76fd55c70ac5755e86d6aa4
Elle Mouton <elle.mouton@gmail.com>
2025-01-04 14:33:24 +0200

    accounts: add new IncreaseAccountBalance Store method
    And use it instead of UpdateAccount in the InterceptorService's
    invoiceUpdate method.

--- accounts/store_kvdb.go
+++ accounts/store_kvdb.go
@@ -233,6 +233,22 @@ func (s *BoltStore) AddAccountInvoice(_ context.Context, id AccountID,
 	return s.updateAccount(id, update)
 }
 
+// IncreaseAccountBalance increases the balance of the account with the given ID
+// by the given amount.
+//
+// NOTE: This is part of the Store interface.
+func (s *BoltStore) IncreaseAccountBalance(_ context.Context, id AccountID,
+	amount lnwire.MilliSatoshi) error {
+
+	update := func(account *OffChainBalanceAccount) error {
+		account.CurrentBalance += int64(amount)
+
+		return nil
+	}
+
+	return s.updateAccount(id, update)
+}
+
 func (s *BoltStore) updateAccount(id AccountID,
 	updateFn func(*OffChainBalanceAccount) error) error {
 
