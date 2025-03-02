81b3da54262b8b75eb6144285d74acc03cb4f7f5
Elle Mouton <elle.mouton@gmail.com>
2025-03-02 08:36:51 +0200

    session: fix test timestamp

--- session/store_test.go
+++ session/store_test.go
@@ -440,7 +440,7 @@ func reserveSession(db Store, label string,
 
 	return db.NewSession(
 		context.Background(), label, opts.sessType,
-		time.Date(99999, 1, 1, 0, 0, 0, 0, time.UTC),
+		time.Date(9999, 1, 1, 0, 0, 0, 0, time.UTC),
 		"foo.bar.baz:1234", options...,
 	)
 }
